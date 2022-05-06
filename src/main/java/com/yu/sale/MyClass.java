f1e593a3d312304519611d30b6b54807039a65f2
Yuchun0221 <92795387+Yuchun0221@users.noreply.github.com>
2022-05-06 14:08:26 +0800

    interface test

---
+++ src/main/java/com/yu/sale/MyClass.java
@@ -0,0 +1,13 @@
+package com.yu.sale;
+
+public class MyClass implements MyInterface {
+    @Override
+    public void test() {
+
+    }
+
+    @Override
+    public int max(int a, int b) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/yu/sale/MyInterface.java
@@ -0,0 +1,6 @@
+package com.yu.sale;
+
+public interface MyInterface {
+    public void test();
+    public int max(int a,int b);
+}
