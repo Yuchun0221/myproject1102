1148753d417eb573e754984778972e15fa2dc6af
Yuchun0221 <92795387+Yuchun0221@users.noreply.github.com>
2022-05-13 16:07:16 +0800

    fee

--- src/main/java/com/yu/parking/Runner.java
+++ src/main/java/com/yu/parking/Runner.java
@@ -8,10 +8,11 @@ public class Runner {
     public static void main(String[] args) {
         String id="ABC-1234";
         String enterTime="08:00";
-        String existTime="11:00";
+        String existTime="11:15";
         SimpleDateFormat sdf=new SimpleDateFormat("HH:mm");
+        Date enterDate=null;
         try{
-            Date enterDate=sdf.parse(enterTime);
+            enterDate=sdf.parse(enterTime);
             System.out.println(enterDate);
             System.out.println(enterDate.getTime());
         }catch (Exception a){
@@ -19,8 +20,20 @@ public class Runner {
         }
         try{
             Date existDate=sdf.parse(existTime);
-            System.out.println(existDate);
-            System.out.println(existDate.getTime());
+//            System.out.println(existDate);
+//            System.out.println(existDate.getTime());
+            int mins=(int)(existDate.getTime()-enterDate.getTime())/1000/60;
+            System.out.println(mins);
+            int fee=0;
+            if (mins%60<0.25){
+                fee=30*(mins/60);
+            }if(0.25<=mins%60&&mins<0.5){
+                fee=30*(mins/60+1/2);
+            }
+            else {
+                fee=30*(mins/60+1);
+            }
+            System.out.println("Fee"+fee);
         }catch (Exception b){
             System.out.println("error");
         }
