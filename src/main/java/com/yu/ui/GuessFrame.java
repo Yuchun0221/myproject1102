f25d19da25c566197227eae161ab7f89a5dea30a
Yuchun0221 <hyc1327@gmail.com>
2022-05-27 15:08:37 +0800

    label

---
+++ src/main/java/com/yu/ui/GuessFrame.java
@@ -0,0 +1,41 @@
+package com.yu.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GuessFrame extends JFrame {
+    public GuessFrame(){
+        super();
+        setSize(600,400);
+        setLocation(700,300);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        JButton  button=new JButton("Hi");
+        JLabel label=new JLabel("hahaha");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("print");
+                label.setText("Hello");
+            }
+        });
+
+        setLayout(new FlowLayout());
+        add(button);
+        add(label);
+        setVisible(true);
+
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        GuessFrame guessFrame=new GuessFrame();
+//      guessFrame.setSize(600,400);
+//      guessFrame.setVisible(true);
+    }
+}
+
---
+++ src/main/java/com/yu/uipratice/WinPratice.java
@@ -0,0 +1,33 @@
+package com.yu.uipratice;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class WinPratice {
+    public static void main(String[] args) {
+        JFrame frame=new JFrame();
+        frame.setLocation(800,300);
+        frame.setSize(300,300);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JButton button=new JButton("button");
+//        PraticeActionListener listener=new PraticeActionListener();
+//        button.addActionListener(listener);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("taped");
+            }
+        });
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Hi");
+            }
+        });
+        frame.setLayout(new FlowLayout());
+        frame.add(button);
+        frame.setVisible(true);
+    }
+}
