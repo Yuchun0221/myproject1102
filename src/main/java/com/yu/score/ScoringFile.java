1dd8e3bdc5bbd9177ebaa2592f2224bde82ca79d
Yuchun0221 <hyc1327@gmail.com>
2022-06-17 16:26:48 +0800

    readline

--- src/main/java/com/yu/score/ScoringFile.java
+++ src/main/java/com/yu/score/ScoringFile.java
@@ -1,5 +1,6 @@
 package com.yu.score;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,9 +9,23 @@ public class ScoringFile {
     public static void main(String[] args) {
         try{
             FileReader fileReader = new FileReader("scores.txt");
-            int data = fileReader.read();
-            System.out.println(data);
-        }catch (FileNotFoundException e){
+            BufferedReader reader = new BufferedReader(fileReader);
+            /*int data = fileReader.read();
+            while (data !=-1 ) {
+                System.out.print((char) data);
+                data = fileReader.read();*/
+            String line = reader.readLine();
+            while(line != null){
+                //System.out.println(line);
+                String[] token = line.split(",");
+                String name = token[0];
+                int english = Integer.parseInt(token[1]);
+                int math = Integer.parseInt(token[2]);
+                Student stu = new Student(name,english,math);
+                stu.print();
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e){
             System.out.println("File error");
         }catch (IOException e){
             System.out.println("source error");
